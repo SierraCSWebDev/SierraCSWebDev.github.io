d4e9efb1e6a1f39c62c2dde4d759e918ef052666
Skootchi <161168960+5arai@users.noreply.github.com>
2024-04-17 19:38:13 -0700

    Add files via upload

---
+++ src/Mission.jsx
@@ -0,0 +1,73 @@
+import './mission.css';
+
+
+function Blog(props){
+    return(
+
+        <>
+            <div className='blog-label'>
+                <img src={props.pic}/>
+                <h3>{props.heading}</h3>
+                <div>
+                    <h3>{props.date}</h3>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+function Mission(){
+
+    return(
+        <>
+
+            <div className='background-pic'>
+                    <h1>WELCOME TO OUR CLUB</h1>
+            </div>
+            
+            <div>
+                <h1 id='headline'>This Website is Still in Development.</h1>
+            </div>
+
+            <div className='news'>
+
+                <div className="news-box">
+                    <div className='title'>
+                        <h2>Meeting Schedule</h2>
+                    </div>
+                    <div className='info'>
+                        <h2>
+                            Friday at 2PM
+                            <br/>
+                            Math Center (V-319)
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            Join the Cult
+                            </h2>
+                        <a href="https://discord.gg/4CUhxRnsty" target='_blank'><img src="icons8-discord.svg"/></a>
+                            
+                    </div>
+                </div>
+
+                <div className="news-box">
+                    <div className='title'>
+                        <h2>Latest Blog Post</h2>
+                    </div>
+                    <div className='blog'>
+                        <Blog pic="/club-icon.svg" heading="Computer Science Club Has Returned!" date="02/04"/>
+                        <Blog pic="/odie.jpg" heading="Odie Got Adopted!" date="03/03"/>
+                        <h2>Upcoming Events!</h2>
+                        <Blog pic="https://cdn1.iconfinder.com/data/icons/metro-ui-dock-icon-set--icons-by-dakirby/512/Intel.png"
+                            heading="Two Guests From Intel Is Attending Our Club Meeting from 1:00 - 2:00 pm" date="04/19"/>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Mission;
---
+++ src/card.jsx
@@ -0,0 +1,90 @@
+import './App.css';
+import './card.css';
+
+function Member(props) {
+    return(
+        <>
+            <div className="member-card">
+            
+                <img src={props.pfp}/>
+                <h3>{props.name}</h3>
+            
+            </div>
+        </>
+    )
+}
+
+function Statement(props){
+    return(
+        <>
+            <div className='statement'>
+                <img src={props.pfp}/>
+                <h2>{props.name}</h2>
+                <h4>{props.statement}</h4>
+            </div>
+        </>
+    )
+}
+
+function Card() {
+    return(
+        <>
+            <div className='team'>
+                <h1>MEET THE TEAM</h1>
+            </div>
+
+            <div className='mission'>
+                <h1>Our Mission</h1>
+                <p>
+                    The Sierra College Computer Science Club (SCCSC) brings together and empowers students interested
+                    in Computer Science and other related majors. We highly encourage students coming from diverse
+                    backgrounds to consider this opportunity to get involved in their community, increase their chances
+                    at internships, and hear the stories of guest speakers in the profession.
+                </p>
+            </div>
+
+            
+
+            <div id='wwd'>
+                <h1>What We Do:</h1>
+                <h3>Creating community, opportunity, and programming experience for individuals interested in Computer Science</h3>
+                <ul>
+                    <li><h3>Workshops</h3></li>
+                    <li><h3>Guest Speakers</h3></li>
+                    <li><h3>Competitions</h3></li>
+                    <li><h3>Social Events</h3></li>
+                </ul>
+            </div>
+
+
+            <div className="card">
+                <h1 className="heading">Members</h1>
+                <div className="member">
+                    <Member pfp="/dv.png" name="DV"/>
+                    <Member pfp="/dhalia.jpg" name="Dahlia"/>
+                    <Member pfp="/theresa.png" name="Theresa"/>
+                    <Member pfp="/ben.jpg" name="Ben"/>
+                    <Member pfp="/vicky.jpg" name="Vicky"/>
+                    <Member pfp="/shawyan.jpg" name="Shawyan"/>
+                    <Member pfp="/sparsh.png" name="Sparsh"/>
+                    <Member pfp="/marcus.jpg" name="Marcus"/>
+                    <Member pfp="/seaven.jpg" name="Seaven"/>
+                    <Member pfp="/club-icon.svg" name="Justin"/>
+                </div>
+            </div>
+
+            <div className='Statements'>
+                <Statement pfp="./neil.jpeg" name="Neil" statement="Create a Computer Science community at Sierra."/>
+                <Statement pfp="./jason.jpg" name="Jason" statement="Share knowledge and challenge each other in new ways of thinking."/>
+                <Statement pfp="./club-icon.svg" name="Austin" statement="Allow Computer Science students the opportunity to create personal projects."/>
+                <Statement pfp="./club-icon.svg" name="Alan Turing" statement="Allow Computer Science students to gain experience working in a team."/>
+                <Statement pfp="./club-icon.svg" name="Nathan" statement="Building professional networking connections with industry professionals and alumni."/>
+                <Statement pfp="/theresa.png" name="Theresa" statement="Fostering curiosity, ingenuity, and creativity in students 
+                about Computer Science through open opportunities."/>
+            </div>
+            
+        </>
+    );
+}
+
+export default Card
